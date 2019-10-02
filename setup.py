98cdc5fe0409f25dcafc2c77c04fc0343a7cef49
Savagecoder77 <savagecoder77@gmail.com>
2019-10-02 08:31:44 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+# Get Readme text
+with open('README.rst') as f:
+    readme = f.read()
+
+
+# Get License text
+with open('LICENSE') as f:
+    license = f.read()
+
+
+# Run setup
+setup(
+    name='Marvin-V4',
+    version='0.0.0',
+    description='Virtual Assistant using Python',
+    long_description=readme,
+    author='Rafael Cenzano',
+    author_email='savagecoder77@gmail.com',
+    url='https://github.com/Marvin-Virtual-Assistant/Marvin-V4',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
