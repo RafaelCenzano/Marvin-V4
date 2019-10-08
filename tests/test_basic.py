66d044fd8310baff5eaa72a327c083896ec17d01
Savagecoder77 <savagecoder77@gmail.com>
2019-10-08 16:38:07 -0700

    remove context file and move imports to init

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,4 @@
-from .context import marvin
+from . import marvin
 
 import unittest
 
@@ -6,9 +6,11 @@ import unittest
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
+    # simple test to show that test system works
     def test_absolute_truth_and_meaning(self):
         assert True
 
+    # practice test
     def test_core_func(self):
     	self.assertEqual(marvin.core.func(), "hello")
 
