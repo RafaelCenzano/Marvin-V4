53be1f781866611f712c56963d68e40be0f9c47e
Savagecoder77 <savagecoder77@gmail.com>
2019-10-13 10:14:27 -0700

    remove class function, function moved. remove uneeded rounding. Add new function

--- marvin/helpers/physics_helpers/kinematics.py
+++ marvin/helpers/physics_helpers/kinematics.py
@@ -18,15 +18,15 @@ class Kinematics:
         sigFigsList = []
 
         if self.initialVelocity != None:
-            sigFigsList.append(self.count_sig_figs(self.initialVelocity))
+            sigFigsList.append(count_sig_figs(self.initialVelocity))
         if self.finalVelocity != None:
-            sigFigsList.append(self.count_sig_figs(self.finalVelocity))
+            sigFigsList.append(count_sig_figs(self.finalVelocity))
         if self.time != None:
-            sigFigsList.append(self.count_sig_figs(self.time))
+            sigFigsList.append(count_sig_figs(self.time))
         if self.accelertaion != None:
-            sigFigsList.append(self.count_sig_figs(self.accelertaion))
+            sigFigsList.append(count_sig_figs(self.accelertaion))
         if self.deltaDistance != None:
-            sigFigsList.append(self.count_sig_figs(self.deltaDistance))
+            sigFigsList.append(count_sig_figs(self.deltaDistance))
 
         tempCount = sigFigsList[0]
 
@@ -35,18 +35,7 @@ class Kinematics:
                 tempCount = item
 
         self.sigFigs = tempCount
-        '''
-        if self.initialVelocity != None:
-            self.initialVelocity = self.properRounding(self.initialVelocity, self.sigFigs)
-        if self.finalVelocity != None:
-            self.finalVelocity = self.properRounding(self.finalVelocity, self.sigFigs)
-        if self.time != None:
-            self.time = self.properRounding(self.time, self.sigFigs)
-        if self.accelertaion != None:
-            self.accelertaion = self.properRounding(self.accelertaion, self.sigFigs)
-        if self.deltaDistance != None:
-            self.deltaDistance = self.properRounding(self.deltaDistance, self.sigFigs)
-        '''
+        
     def finalVelocityOne(self):
         '''
         Equation:
@@ -56,11 +45,11 @@ class Kinematics:
         Solve for Final Velocity with initial velocity, acceleration, and time
         '''
 
-        if self.checkValue(self.initialVelocity) and self.checkValue(self.accelertaion) and self.checkValue(self.time):
+        if self.checkValue(self.initialVelocity) and self.checkValue(self.accelertaion) and self.checkValue(self.time) and self.finalVelocity == None:
 
             answer = self.initialVelocity + (self.accelertaion * self.time)
 
-            self.finalVelocity = round(answer, self.sigFigs)
+            self.finalVelocity = answer
 
     def deltaDistanceOne(self):
         '''
@@ -70,11 +59,29 @@ class Kinematics:
 
         Solve for Delta Distance(Displacment) with initial velocity, acceleration, and time
         '''
-        if self.checkValue(self.initialVelocity) and self.checkValue(self.accelertaion) and self.checkValue(self.time):
+        if self.checkValue(self.initialVelocity) and self.checkValue(self.accelertaion) and self.checkValue(self.time) and self.deltaDistance == None:
 
-        	answer = (self.initialVelocity * self.time) + ( 0.5 * self.accelertaion * (self.time ** 2))
+        	answer = (self.initialVelocity * self.time) + (0.5 * self.accelertaion * (self.time ** 2))
 
-        	self.deltaDistance = round(answer, self.sigFigs)
+        	self.deltaDistance = answer
+
+    def finalVelocityTwo(self):
+        '''
+        Equation:
+
+        Vf^2 = Vi^2 + 2 * a * t^2
+
+        Solve for Final Velocity with initial velocity, acceleration, and delta distance
+        '''
+
+        if self.checkValue(self.initialVelocity) and self.checkValue(self.accelertaion) and self.checkValue(self.deltaDistance) and self.finalVelocity == None:
+
+            answer = (self.initialVelocity ^ 2) + (2 * self.accelertaion * self.deltaDistance)
+
+            answerSqrt = Math.sprt(answer)
+
+            self.finalVelocity = answerSqrt
+
 
     def checkValue(self, value):
         '''
@@ -92,8 +99,5 @@ class Kinematics:
 if __name__ == '__main__':
     tester = Kinematics(initialVelocity=0, accelertaion=9.8, time=5.7373)
     tester.deltaDistanceOne()
-    #print(tester.sigFigs)
-    #print(tester.deltaDistance)
-    num = tester.properRounding(5737390)
-    print(type(num))
-    print(num)
+    print(tester.sigFigs)
+    print(tester.deltaDistance)
