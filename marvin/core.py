43e32e5b1ea25d1635daba086cea24ce97ffd3ab
Savagecoder77 <savagecoder77@gmail.com>
2019-10-20 12:21:18 -0700

    flask app beginings

--- marvin/core.py
+++ marvin/core.py
@@ -1,2 +1,11 @@
-# Imports
-import marvin.helpers
+# Flask imports
+from flask import Flask
+
+# Create Flask app
+app = Flask(__name__)
+
+# Add Configurations to app
+app.config.from_pyfile('marvin/config.py', silent=True)
+
+# Import all views
+import marivn.gui.views
