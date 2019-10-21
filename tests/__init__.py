2f05ea09d7e614752a85d806dfbf988cecdd49fc
Savagecoder77 <savagecoder77@gmail.com>
2019-10-21 11:43:35 -0700

    add system path for helper folder and properly import physics helpers

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../marvin/helpers')))
 
 import marvin
-import marvin.helpers.physics_helpers
+import physics_helpers
