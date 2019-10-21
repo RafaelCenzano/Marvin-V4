8a13ec36f7a0a901bddf14ba298a81f5af35cbad
Savagecoder77 <savagecoder77@gmail.com>
2019-10-21 11:43:04 -0700

    remove test, test function

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,10 +11,6 @@ class BasicTestSuite(unittest.TestCase):
     def test_absolute_truth_and_meaning(self):
         assert True
 
-    # practice test
-    def test_core_func(self):
-    	self.assertEqual(marvin.core.func(), "hello")
-
     def test_sig_fig_counter(self):
     	self.assertEqual(physics_helpers.numberProcessing.count_sig_figs(5), 1)
 
