c57a60021d2013ca6f4154384d4af9b89f9c46fe
Savagecoder77 <savagecoder77@gmail.com>
2019-10-23 11:47:27 -0700

    create and pass tests

--- tests/test_kinematics.py
+++ tests/test_kinematics.py
@@ -6,8 +6,19 @@ import unittest
 
 class BasicTestSuite(unittest.TestCase):
 
-    def test_kinematics_one(self):
-        classObjecttoTest = physics_helpers.kinematics.Kinematics()
+    classObjecttoTest = physics_helpers.kinematics.Kinematics(initialVelocity=3,accelertaion=2,time=20)
+    classObjecttoTest.calculations()
+
+    def test_kinematics_one_vi(self):
+        self.assertEqual(self.classObjecttoTest.initialVelocity, 3)
+    def test_kinematics_one_vf(self):
+        self.assertEqual(self.classObjecttoTest.finalVelocity, 40)
+    def test_kinematics_one_a(self):
+        self.assertEqual(self.classObjecttoTest.accelertaion, 2)
+    def test_kinematics_one_t(self):
+        self.assertEqual(self.classObjecttoTest.time, 20)
+    def test_kinematics_one_d(self):
+        self.assertEqual(self.classObjecttoTest.deltaDistance, 500)
 
 
 if __name__ == '__main__':
