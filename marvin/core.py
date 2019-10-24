f255dcb4e877e72940727511afa5da2b252815cc
Savagecoder77 <savagecoder77@gmail.com>
2019-10-24 08:34:08 -0700

    add code to core as views

--- marvin/core.py
+++ marvin/core.py
@@ -1,11 +1,21 @@
-# Flask imports
-from flask import Flask
+from marvin import app, helpers
+from flask import render_template, redirect, url_for
 
-# Create Flask app
-app = Flask(__name__)
+'''
+Views
+'''
+@app.route("/", methods=['GET'])
+def index():
+    return render_template('index.html')
 
-# Add Configurations to app
-app.config.from_pyfile('marvin/config.py', silent=True)
 
-# Import all views
-import marivn.gui.views
+'''
+Error Handelers
+'''
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
