ef586063f4995cffbf5e823e4f8264c162c25537
Savagecoder77 <savagecoder77@gmail.com>
2019-10-24 08:02:04 -0700

    add flask

--- marvin/__init__.py
+++ marvin/__init__.py
@@ -1,1 +1,13 @@
 from marvin import core
+
+# Flask imports
+from flask import Flask
+
+# Create Flask app
+app = Flask(__name__)
+
+# Add Configurations to app
+app.config.from_pyfile('config.py', silent=True)
+
+# Import all views
+import personal_site.views
