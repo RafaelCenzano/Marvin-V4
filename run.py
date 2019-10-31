794448cb2fff51161d980703339f0fa4e510e1f7
Savagecoder77 <savagecoder77@gmail.com>
2019-10-30 17:24:39 -0700

    change to debug and open gui webpage 2 seconds after app start

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
-import marvin
+from webbrowser import open as webopen
+from time import sleep as delay
+from threading import Thread
+from marvin import app
 
 '''
 Run Flask App for Marvin GUI
 '''
-marvin.app.run(
-    port=9090, # assign to port 5000
-    debug=True # Have debug pages show when there is an error
-)
+
+def startGUI():
+    delay(2)
+    webopen('http://127.0.0.1:9090/')
+
+if __name__ == '__main__':
+    thread_start = Thread(target=startGUI)
+    thread_start.start()
+
+    app.run(
+        port=9090, # assign to port 5000
+    )
