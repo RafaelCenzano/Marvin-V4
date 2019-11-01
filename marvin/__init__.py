43086c7fca86635faf1f84e2fc39a9c11d7b8173
Savagecoder77 <savagecoder77@gmail.com>
2019-11-01 14:31:45 -0700

    change to config object

--- marvin/__init__.py
+++ marvin/__init__.py
@@ -1,11 +1,12 @@
 # Flask imports
 from flask import Flask
+from config import Config
 
 # Create Flask app
 app = Flask(__name__)
 
 # Add Configurations to app
-app.config.from_pyfile('config.py', silent=True)
+app.config.from_object(Config)
 
 # Import all views
 import marvin.core
