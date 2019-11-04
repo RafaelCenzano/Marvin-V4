c026ff5785bad32c7cd4839ee0d69d9a66c9bcd1
Savagecoder77 <savagecoder77@gmail.com>
2019-11-03 16:03:13 -0800

    new vector class

---
+++ marvin/helpers/physics_helpers/vectors.py
@@ -0,0 +1,14 @@
+# Imports
+import math
+from marvin.helpers.physics_helpers.numberProcessing import count_sig_figs, properRounding, checkValue, cleanValue
+
+'''
+Kinematics
+
+Functions to solve time, velocity, accelertaion and distance with the other variables stated.
+'''
+
+class Vectors:
+
+	def __init__():
+		pass
