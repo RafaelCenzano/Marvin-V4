bea4224a105ea08fedbc84df385998bcba5a2636
Savagecoder77 <savagecoder77@gmail.com>
2019-11-06 08:21:37 -0800

    pep 8 cleanup

--- marvin/core.py
+++ marvin/core.py
@@ -13,10 +13,12 @@ Views
 def index():
     return render_template('index.html')
 
+
 @app.route('/calculators', methods=['GET'])
 @app.route('/calculators/', methods=['GET'])
 def calculators():
     return render_template('calculators.html')
+
 
 @app.route('/calculators/kinematics', methods=['GET', 'POST'])
 @app.route('/calculators/kinematics/', methods=['GET', 'POST'])
@@ -28,38 +30,38 @@ def kinematics():
             try:
                 temp = float(form.vi.data)
                 count += 1
-            except:
+            except BaseException:
                 flash('Initial Velocity must be a number', 'warning')
         if form.vf.data is not None:
             try:
                 temp = float(form.vf.data)
                 count += 1
-            except:
+            except BaseException:
                 flash('Final Velocity must be a number', 'warning')
         if form.t.data is not None:
             try:
                 temp = float(form.t.data)
                 count += 1
-            except:
+            except BaseException:
                 flash('Time must be a number', 'warning')
         if form.a.data is not None:
             try:
                 temp = float(form.a.data)
                 count += 1
-            except:
+            except BaseException:
                 flash('Acceleration must be a number', 'warning')
         if form.d.data is not None:
             try:
                 temp = float(form.d.data)
                 count += 1
-            except:
+            except BaseException:
                 flash('Delta Distance must be a number', 'warning')
         if count >= 3:
             physicsdata = physics_helpers.kinematics.Kinematics(
                 physics_helpers.numberProcessing.formCleanup(form.vi.data),
                 physics_helpers.numberProcessing.formCleanup(form.vf.data),
                 physics_helpers.numberProcessing.formCleanup(form.t.data),
-                physics_helpers.numberProcessing.formCleanup(form.a.data), 
+                physics_helpers.numberProcessing.formCleanup(form.a.data),
                 physics_helpers.numberProcessing.formCleanup(form.d.data))
             physicsdata.calculations()
             flash('Successfully calculated!', 'success')
@@ -69,19 +71,22 @@ def kinematics():
         flash('You need to input at least 3 givens', 'error')
     return render_template('kinematics.html', form=form)
 
+
 @app.route('/calculators/sigfigs', methods=['GET', 'POST'])
 @app.route('/calculators/sigfigs/', methods=['GET', 'POST'])
 def sigfigs():
     form = forms.SigFigForm()
     if request.method == 'POST':
         if form.validate():
-            sigFigCount = physics_helpers.numberProcessing.count_sig_figs(form.num.data)
+            sigFigCount = physics_helpers.numberProcessing.count_sig_figs(
+                form.num.data)
             flash('Successfully counted!', 'success')
             return render_template(
                 'sigfigsSuccess.html',
                 sigFigCount=sigFigCount)
         flash('You need to input a value', 'error')
     return render_template('sigfigs.html', form=form)
+
 
 '''
 Error Handlers
