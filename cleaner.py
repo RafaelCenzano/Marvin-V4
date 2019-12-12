5e2dfa79994e53f5b9c69bb75590a91e02d56019
Savagecoder77 <savagecoder77@gmail.com>
2019-12-12 08:27:30 -0800

    new file to clean up and remove pyc and __pycache__ folders

---
+++ cleaner.py
@@ -0,0 +1,26 @@
+import os
+
+def cacheCleaner():
+    pathsToRemoveFiles = []
+    pathsToRemoveDirs = []
+    pathToFolder = os.getcwd()
+
+    for (root,dirs,files) in os.walk('.', topdown=True):
+        dirsChecking = root[2:]
+        if root[2:6] != 'venv':
+            for directories in dirs:
+                if '__pycache__' == directories:
+                    pathsToRemoveDirs.append(os.path.join(pathToFolder,dirsChecking,directories))
+            for f in files:
+                name, ext = os.path.splitext(f)
+                if '.pyc' == ext:
+                    pathsToRemoveFiles.append(os.path.join(pathToFolder,dirsChecking,f))
+    for paths in pathsToRemoveFiles:
+        if os.path.exists(paths):
+            os.remove(paths)
+    for paths in pathsToRemoveDirs:
+        if os.path.exists(paths):
+            os.rmdir(paths)
+
+if __name__ == '__main__':
+    cacheCleaner()
