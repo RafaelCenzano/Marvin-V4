84f5d7327ed1d0b25b41ff69e06d549a4aae224c
Savagecoder77 <savagecoder77@gmail.com>
2020-01-10 13:16:10 -0800

    remove kivy code

--- marvin/core.py
+++ marvin/core.py
@@ -1,10 +1,1 @@
 from marvin.helpers import physics
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-
-class MarvinApp(App):
- 
-    def build(self):
- 
-        return Label(text="Hello Kivy!")
