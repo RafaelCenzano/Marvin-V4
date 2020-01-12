8c664b85c260c8731c581a516ba23ab15cdc4030
Savagecoder77 <savagecoder77@gmail.com>
2020-01-11 17:45:34 -0800

    add kivy

--- marvin/core.py
+++ marvin/core.py
@@ -1,1 +1,14 @@
 from marvin.helpers import physics
+import kivy
+from kivy.app import App
+from kivy.config import Config
+from kivy.uix.label import Label
+
+Config.set('graphics', 'resizable', 1)
+
+kivy.require('1.11.1')
+
+class MarvinApp(App): 
+   
+    def build(self): 
+        return Label(text='Child')
