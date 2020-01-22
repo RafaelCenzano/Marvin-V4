126a9c2a509399040defd1a080aa370bfb480a5b
Savagecoder77 <savagecoder77@gmail.com>
2020-01-22 13:38:35 -0800

    update to pep 8 standard

--- marvin/helpers/music/music.py
+++ marvin/helpers/music/music.py
@@ -5,6 +5,7 @@ import moviepy.editor
 import os
 import hashlib
 
+
 class MusicProccessor:
 
     def __init__(self):
@@ -15,8 +16,21 @@ class MusicProccessor:
         newVideoName = hashlib.sha224(video.title.encode('utf-8')).hexdigest()
 
         # Paths for files
-        tempPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'marvin', 'static', 'music', 'library', 'temp') + newVideoName + '.mp4'
-        finalPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'marvin', 'static', 'music', 'library') + newVideoName + '.mp3'
+        tempPath = os.path.join(
+            os.path.abspath(
+                os.path.dirname(__file__)),
+            'marvin',
+            'static',
+            'music',
+            'library',
+            'temp') + newVideoName + '.mp4'
+        finalPath = os.path.join(
+            os.path.abspath(
+                os.path.dirname(__file__)),
+            'marvin',
+            'static',
+            'music',
+            'library') + newVideoName + '.mp3'
 
         # Proccess and download videos
         video = pafy.new(links)
@@ -30,10 +44,25 @@ class MusicProccessor:
         os.remove(tempPath)
 
     def writeDataFile(self, link, bestlink, videoName):
-        dataPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'marvin', 'static', 'music', 'library', 'data') + videoName + '.marvin'
+        dataPath = os.path.join(
+            os.path.abspath(
+                os.path.dirname(__file__)),
+            'marvin',
+            'static',
+            'music',
+            'library',
+            'data') + videoName + '.marvin'
 
     def readDataFile(self, videoName):
-        dataPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'marvin', 'static', 'music', 'library', 'data') + videoName + '.marvin'
+        dataPath = os.path.join(
+            os.path.abspath(
+                os.path.dirname(__file__)),
+            'marvin',
+            'static',
+            'music',
+            'library',
+            'data') + videoName + '.marvin'
+
 
 class PlaylistProcessor(MusicProccessor):
 
@@ -54,14 +83,33 @@ class PlaylistProcessor(MusicProccessor):
 
     def writePlaylistFile(self, playlistName):
         playlistName = self.cleanName(playlistName)
-        os.path.isfile(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'marvin', 'static', 'music', 'playlist', playlistName + '.marvin'))
+        os.path.isfile(
+            os.path.join(
+                os.path.abspath(
+                    os.path.dirname(__file__)),
+                'marvin',
+                'static',
+                'music',
+                'playlist',
+                playlistName +
+                '.marvin'))
 
     def readPlaylistFile(self, playlistName):
         playlistName = self.cleanName(playlistName)
-        os.path.isfile(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'marvin', 'static', 'music', 'playlist', playlistName + '.marvin'))
+        os.path.isfile(
+            os.path.join(
+                os.path.abspath(
+                    os.path.dirname(__file__)),
+                'marvin',
+                'static',
+                'music',
+                'playlist',
+                playlistName +
+                '.marvin'))
 
     def cleanName(self, name):
-        # Add marvin in front to make sure all file names start with something consistent that won't mess with things
+        # Add marvin in front to make sure all file names start with something
+        # consistent that won't mess with things
         cleanedName = 'marvin'
 
         # loop through and replace spaces with '_'
@@ -75,6 +123,7 @@ class PlaylistProcessor(MusicProccessor):
             count += 1
 
         return cleanedName
+
 
 if __name__ == '__main__':
     pass
