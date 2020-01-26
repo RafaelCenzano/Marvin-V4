ba0fd8607b6dc39d51950c1fe192e84f8d9cdfa2
Savagecoder77 <savagecoder77@gmail.com>
2020-01-25 16:17:35 -0800

    bring 404 error page

--- marvin/core.py
+++ marvin/core.py
@@ -190,9 +190,13 @@ def back():
 
 '''
 Error Handlers
+'''
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+'''
 @app.errorhandler(500)
 def server_error(e):
     return render_template('500.html'), 500
